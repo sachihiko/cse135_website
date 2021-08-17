febe58aaf89211e6c153eada9ffbb9ba11371619
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-16 20:14:44 -0700

    edited .c files

--- cgi-bin/c-hello-html-world.c
+++ cgi-bin/c-hello-html-world.c
@@ -9,11 +9,11 @@ int main(void)
   // Print HTML header
   printf("Cache-Control: no-cache\n");
   printf("Content-type: text/html\n\n");
-  printf("<html><head><title>Hello CGI World</title></head>\
+  printf("<html><head><title>Sachi Hello CGI World</title></head>\
 	<body><h1 align=center>Hello HTML World</h1>\
   	<hr/>\n");
 
- printf("Hello World<br/>\n");
+ printf("Hello Sachi<br/>\n");
  printf("This program was generated at: %s\n<br/>", ctime(&t));
  printf("Your current IP address is: %s<br/>", getenv("REMOTE_ADDR"));
  
