a298b93b54ae8355452c67cbc73cd206bf0c5ba5
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 15:37:21 -0700

    added post python

---
+++ cgi-bin/py-post-echo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os
+import sys
+from urllib.parse import parse_qsl
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n")
+print("<html><head><title>POST Request Echo</title></head>"
+	"<body><h1 align=center>POST Request Echo</h1><hr/>")
+print(f"Message Body: {sys.stdin.read()}</br>")
+print("</body>")
+print("</html>")
