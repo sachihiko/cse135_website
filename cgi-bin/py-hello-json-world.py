71a960223c9e2e6f838da6545e6ce443c3ee2f8d
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 01:41:02 -0700

    added  python json

---
+++ cgi-bin/py-hello-json-world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import html
+import os
+import datetime
+
+now = datetime.datetime.now()
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+client_ip = html.escape(os.environ["REMOTE_ADDR"])
+
+print("Cache-Control: no-cache")
+print("Content-type: application/json\n")
+print("{\n\t\"message\": \"Hello Sachi\",")
+print(f"\t\"date\": \"{current_time}\"")
+print(f"\t\"currentIP\": \"{client_ip}\"")
+print("}")
