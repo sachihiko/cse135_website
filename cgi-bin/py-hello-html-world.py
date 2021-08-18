947c08e2cf1168fc659cfa32f5f8661cc9778f40
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 01:17:37 -0700

    python cgi works i think

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-import socket
+import request
 import datetime
 
 now = datetime.datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
+client_ip = request.environ.get('REMOTE_ADDR')
+print(f'''
+<html>
+<head><title>Hello World - plz work</title></head>
+<body>
+<body><h1 align=center>Python says hello world</h1><hr/>
 
-print("Cache-Control: no-cache\n")
-print("Content-type: text/html\n\n")
-print("<html><head><title> Hello World</title></head>"
-    "<body><h1 align=center>Hello Python World</h1>"
-    "<hr/>\n")
-
-print("Hello World<br/>\n")
-print(f"This program was generated at: {current_time}\n<br/>")
-print(f"Your current IP address is: {ip}<br/>")
-print("</body></html>")
+This program was generated at: {current_time}<br/>
+Your current IP address is: {client_ip}<br/>
+</body>
+</html>
+''')
