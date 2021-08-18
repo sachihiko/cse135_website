da3f36e78e63cda1e23b092b789becb1fc2d5e13
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 00:16:50 -0700

    added python hello world cgi

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,18 @@
+import socket
+import datetime
+
+now = datetime.datetime.now()
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+print("Cache-Control: no-cache\n")
+print("Content-type: text/html\n\n")
+print("<html><head><title> Hello World</title></head>"
+    "<body><h1 align=center>Hello Python World</h1>"
+    "<hr/>\n")
+
+print("Hello World<br/>\n")
+print(f"This program was generated at: {current_time}\n<br/>")
+print(f"Your current IP address is: {ip}<br/>")
+print("</body></html>")
