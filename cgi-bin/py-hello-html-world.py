ff3488d430a07b6a351e7d01875a53ec783800ef
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 01:28:08 -0700

    python cgi works i think

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -6,6 +6,9 @@ import datetime
 now = datetime.datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 client_ip = html.escape(os.environ["REMOTE_ADDR"])
+print('Cache-Control: no-cache')
+print('Content-type: text/html')
+print()
 print(f'''
 <html>
 <head><title>Hello World - plz work</title></head>
