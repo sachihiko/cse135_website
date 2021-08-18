e6d49ed5d17cc4dba7f2976a0e3580f9ea984831
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 16:02:06 -0700

    gen req py

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import sys
 # from urllib.parse import parse_qsl
@@ -6,8 +7,8 @@ print("Cache-Control: no-cache")
 print("Content-type: text/html\n")
 print("<html><head><title>General Request Echo</title></head>"
 	"<body><h1 align=center>General Request Echo</h1><hr/>")
-print(f"Protocol: {os.environ['SERVER_PROTOCOL']}")
-print(f"Method: {os.environ['REQUEST_METHOD']}")
-print(f"Message Body: {sys.stdin.read()}</br>")
+print(f"Protocol: {os.environ['SERVER_PROTOCOL']}<br/>")
+print(f"Method: {os.environ['REQUEST_METHOD']}<br/>")
+print(f"Message Body: {sys.stdin.read()}<br/>")
 print("</body>")
 print("</html>")
