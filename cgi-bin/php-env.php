14a4f1d82959af31b79e7261d20791c5a0a0a93a
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 21:03:55 -0700

    php env

---
+++ cgi-bin/php-env.php
@@ -0,0 +1,18 @@
+<html>
+    <?php  
+    header("Cache-Control: no-cache\n");
+    header("Content-type: text/html\n\n");
+    ?>
+
+    <head>
+        <title>Environment Variables</title>
+    </head>
+    <body>
+        <h1 align="center">Environment Variables</h1>
+        <?php 
+        foreach ($_ENV as $key => $value) {
+            echo "<b>$key:</b> $value<br/>\n ";
+        }
+        ?>
+    </body>
+</html>
