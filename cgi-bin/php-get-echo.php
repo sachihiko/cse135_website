333d55ff3382ff0a756f8f5a5af23d9fe63ae287
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 21:58:28 -0700

    wan di

--- cgi-bin/php-get-echo.php
+++ cgi-bin/php-get-echo.php
@@ -9,11 +9,13 @@
     <body>
         <h1 align="center">GET Request Echo</h1>
     <?php
-    if ($_ENV['QUERY_STRING']){
-        $buffer = $_ENV['QUERY_STRING']
+    if ($_SERVER['QUERY_STRING']){
+        $buffer = $_SERVER['QUERY_STRING'];
+        echo "Query string: $buffer<br/>";
+        echo "Parsed query string:<br/>";
         parse_str($buffer, $params);
         foreach($params as $key => $value){
-            echo"<b>$key</b>: $value<br/>";
+           echo"<b>$key</b>: $value<br/>";
         }
     }
     ?>
