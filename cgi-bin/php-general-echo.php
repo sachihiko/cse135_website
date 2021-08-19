78bfe005f0d6c1816d70525c1069ba84def9ce3c
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 22:20:55 -0700

    general echo php

---
+++ cgi-bin/php-general-echo.php
@@ -0,0 +1,24 @@
+<html>
+    <?php
+    header("Cache-Control: no-cache\n");
+    header("Content-type: text/html\n\n");
+    ?>
+    <head>
+        <title>POST Request Echo</title>
+    </head>
+    <body>
+        <h1 align="center">POST Request Echo</h1>
+<?php
+        $protocol = $_SERVER['SERVER_PROTOCOL'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        echo "<p><b>HTTP Protocol:</b> $protocol</p>";
+        print "<p><b>HTTP Method:</b> $method</p>";
+        echo "Message Body:<br/>";
+        if ($method){
+        foreach($method as $key => $value){
+           echo"<b>$key</b>: $value<br/>";
+        }
+    }
+    ?>
+    </body>
+</html>
