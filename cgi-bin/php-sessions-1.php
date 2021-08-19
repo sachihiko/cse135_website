381d4a3026170633cfce2f1afad64d52c56e0a85
Sachi Kanda <kandasachihiko@gmail.com>
2021-08-18 23:01:14 -0700

    php sessions 1

--- cgi-bin/php-sessions-1.php
+++ cgi-bin/php-sessions-1.php
@@ -9,11 +9,18 @@
     <body>
         <h1>PHP Sessions</h1>
         <?php
-        $name = 'sachi';
-        #$name = $_POST['username'];
+        $name = $_POST["username"]
+        echo "Your Name: $name";
         setcookie("username", $name);
         echo $_SERVER["HTTP_COOKIE"];
 
         ?>
+        <br/><br/>
+        <a href="/cgi-bin/php-sessions-2.php">Session Page 2</a><br/>
+        <a href="/php-cgiform.html">PHP CGI Form</a><br />
+        <form style="margin-top:30px" action="/cgi-bin/php-destroy-session.php" method="get">
+        <button type="submit">Destroy Session</button>
+        </form>
+
     </body>
 </html>
